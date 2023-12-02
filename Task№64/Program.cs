84873e6603bdd7ede36e31b854be97db8b39216e
Mitrich-rus <146307143+Mitrich-rus@users.noreply.github.com>
2023-12-02 23:23:14 +1000

    initial commit

---
+++ Task№64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N.
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+//  Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+// Усложнение. С шагом 0.5
+
+Console.WriteLine("Введите натуральное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Convert.ToDouble (num);
+int endNum = 1;
+
+static string PrintNumber(double start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+
+    return start + "\t" + PrintNumber(start-0.5, end);
+}
+
+Console.WriteLine(PrintNumber(num,endNum));
---
+++ Task№66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите натуральное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число:");
+int endNum = Convert.ToInt32(Console.ReadLine());
+
+static int SummOfDistance(int start, int end)
+{
+    if (start == end)
+    {
+        return start;
+    }
+    return start + SummOfDistance(start + 1, end);
+}
+
+if (endNum < num)
+{
+    Console.WriteLine($"Сумма натуральных элементов в промежутке от {endNum} до {num} равна "+SummOfDistance(endNum, num));
+}
+if (endNum > num)
+{
+    Console.WriteLine($"Сумма натуральных элементов в промежутке от {num} до {endNum} равна "+SummOfDistance(num, endNum));
+}
+else Console.WriteLine($"Сумма натуральных элементов в промежутке от {num} до {endNum}  равна 0");
